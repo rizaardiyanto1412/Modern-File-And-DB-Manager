931a860696b5dda729ac9f32dfbb9378869f6445
Riza Maulana Ardiyanto <rizaardiyanto01@gmail.com>
2026-03-10 07:14:18 +0700

    Improve filetree move copy modal

--- modern-file-manager.php
+++ modern-file-manager.php
@@ -3,7 +3,7 @@
  * Plugin Name: Modern File Manager
  * Description: Secure and modern file manager for WordPress admins.
  * Version: 1.0.0
- * Author: Modern File Manager Team
+ * Author: Camyt
  * License: GPL-2.0-or-later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: modern-file-manager
